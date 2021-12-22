ac9511989eaf5e0f99aa571086d09e22e35c7ffc
Hanz Uyonco <hmuyonco@up.edu.ph>
2021-12-23 01:16:37 +0800

    Debug BTW comments

--- src/utils/lexicalAnalysis.js
+++ src/utils/lexicalAnalysis.js
@@ -9,14 +9,14 @@ const lexicalAnalysis = (code) =>{
             words.push(wordArray[j])
         }
     }
-    console.log(words)
+    // console.log(words)
     let lexemes = []
     let varAssignFlag = false
     let wordsHolder = ""
     let yarnAssignFlag = false
     let commentAssignFlag = false
     for (let i = 0; i < words.length; i++){
-        if (words[i] === "I") {
+        if (words[i] === "I" || words[i] === "\tI") {
             if (words[i+1] === "HAS" && words[i+2] === "A"){
                 varAssignFlag = true
                 wordsHolder = wordsHolder.concat(words[i], " ", words[i+1], " ", words[i+2])
@@ -28,11 +28,25 @@ const lexicalAnalysis = (code) =>{
                 wordsHolder = ""
             }
         }
-        if (words[i].charAt(0) === "\""){
+        if (words[i].charAt(0) === "\"" && !commentAssignFlag){
             yarnAssignFlag = true
         }
+        if (words[i] === "BTW"){
+            const object = {
+                label: words[i],
+                classification: "Comment Delimiter"
+            }
+            lexemes.push(object)
+            commentAssignFlag = true
+        }
 
-        if (!varAssignFlag && !yarnAssignFlag) {
+
+        
+        if (!varAssignFlag && !yarnAssignFlag && !commentAssignFlag) {
+            if (words[i].charAt(0) === "\t"){
+                words[i] = words[i].slice(1)
+            }
+            console.log(words[i])
             for (let j = 0; j < keywords.length; j++){
                 let result = keywords[j].regex.test(words[i])
                 if (result) {
@@ -61,7 +75,21 @@ const lexicalAnalysis = (code) =>{
                 yarnAssignFlag = false
             }
         }
+        if (commentAssignFlag && words[i] !== "BTW") {
+            wordsHolder = wordsHolder.concat(words[i], " ")
+            
+            if (words[i+1]?.charAt(0) === "\t") {
+                console.log(words[i])
 
+                const object = {
+                    label: wordsHolder, 
+                    classification: "Comment"
+                }
+                lexemes.push(object)
+                wordsHolder = ""
+                commentAssignFlag = false
+            }
+        }
     }
     return lexemes
 }
