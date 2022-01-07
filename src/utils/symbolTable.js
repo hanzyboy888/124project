c2736333f22e5590386ea1b5f99a3327f997e583
Hanz Uyonco <hmuyonco@up.edu.ph>
2022-01-07 15:02:36 +0800

    cleaned code

--- src/utils/symbolTable.js
+++ src/utils/symbolTable.js
@@ -1,55 +1,14 @@
 const symbolTable = (lexemes) => {
-
+    
     let symbolTable = {}
-    let visibleFlag = false
-    let visibleArray = []
     let outputs = ""
-    
-    
-    for (let i = 0; i < lexemes.length; i++){
-        /* if (visibleFlag === true){
-            visibleArray.push({
-                value: lexemes[i].label,
-                classification: lexemes[i].classification
-            })
-            for(let x = 0; x < visibleArray.length; x++){
-                for(let y = 0; y < symbolTable.length; y++){
-                    if(visibleArray[x].value === symbolTable[y].identifier){
-                        visibleArray[x].value = symbolTable[y].value
-                    }
-                }
-            }
-            for(let a = 0; a < visibleArray.length; a++){
-                if(visibleArray[a].classification === "YARN Literal"){
-                    visibleArray[a].value = visibleArray[a].value.slice(1, -1)
-                    console.log(visibleArray[a].value)
-                }
-            }
-            
-            if(lexemes[i].classification === "Code Delimiter" || lexemes[i].classification === "Comment Delimiter" || lexemes[i].classification === "Variable Declaration" || lexemes[i].classification === "Variable Assignment" || lexemes[i].classification === "Arithmetic Operation" || lexemes[i].classification === "Boolean Operation" || lexemes[i].classification === "Boolean Infinite Arity Operation" || lexemes[i].classification === "Comparison Operation" || lexemes[i].classification === "Concatenation" || lexemes[i].classification === "Typecast" || lexemes[i].classification === "Input Keyword" || lexemes[i].classification === "Output Keyword" || lexemes[i].classification === "If Operator" || lexemes[i].classification === "Switch Operator" || lexemes[i].classification === "Variable Identifier" || lexemes[i].classification === "Function Identifier" || lexemes[i].classification === "Loop Identifier" || lexemes[i].classification === "Operand Separator"){
-                let temp = ""
-                visibleFlag = false
-                for(let j = 0; j < visibleArray.length; j++){
-                    temp = temp.concat(visibleArray[j].value)
-                }
-                symbolTable.push({
-                    identifier: "VISIBLE",
-                    value: temp
-                })
-                temp = ""
-            }
-        } */
 
+    for (let i = 0; i < lexemes.length; i++){
         if (lexemes[i].classification === "Variable Declaration"){
             symbolTable[lexemes[i + 1].label] = lexemes[i + 3].label;
-            /* symbolTable.push({
-                identifier: lexemes[i+1].label,
-                value: lexemes[i+3].label
-            }) */
             i += 3
         }
         else if (lexemes[i].classification === "Output Keyword"){
-            // visibleFlag = true
             let output = "";
             i++
             for (; i < lexemes.length; i++) {   
@@ -69,6 +28,7 @@ const symbolTable = (lexemes) => {
             outputs += output + "\n"
         }
     }
+    // console.log(symbolTable)
     return [symbolTable, outputs]
 }
 
